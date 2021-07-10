bbf31845a166fc7501810fafce313290d99ce3da
marcela.campos@thoughtworks.com <marcela.campos@thoughtworks.com>
2021-07-10 19:36:27 -0300

    Add render test for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,9 @@
-describe('component tests', () => {
-    it('should render title', () => {
-
+import React from 'react'
+import {render} from '@testing-library/react'
+import App from './App'
+describe('App component tests', () => {
+    it('should render welcome text', () => {
+        const {getByText} = render(<App />);
+        expect(getByText('Bem-vindo à Lojinha!')).toBeInTheDocument(); 
     })
 });
