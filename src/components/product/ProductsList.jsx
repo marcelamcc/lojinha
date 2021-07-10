76a9ae027eab608d363e9c84f345d8b23396804b
marcela.campos@thoughtworks.com <marcela.campos@thoughtworks.com>
2021-07-10 17:38:05 -0300

    fixing lint erros on productsList file

--- src/components/product/ProductsList.jsx
+++ src/components/product/ProductsList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import ImageIcon from '@material-ui/icons/Image';
 
@@ -16,6 +17,10 @@ function ProductsList({ products }) {
       ))}
     </ListBody>
   );
+}
+
+ProductsList.propTypes = {
+  products: PropTypes.array
 }
 
 export default ProductsList;
